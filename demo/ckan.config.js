6d4a59835343046f72b7510bd91446429e3bae1d
Alessio Cimarelli <alessio.cimarelli@ondata.it>
2017-07-01 23:49:30 +0200

    Add CKAN support (alias of DKAN)

---
+++ demo/ckan.config.js
@@ -0,0 +1,9 @@
+window.ES_CONFIG = {
+    backend: "table",
+    options: {
+        table: {
+            type: "CKAN",
+            file: "http://www.confiscatibene.it/it/api/action/datastore/search.json?resource_id=6af9fa81-1421-4bff-8ec4-5528c815cf21"
+        }
+    }
+};
