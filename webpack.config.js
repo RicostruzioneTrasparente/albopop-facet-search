a3293171eb3b964458af536b04ad226c36326ec8
Alessio Cimarelli <alessio.cimarelli@ondata.it>
2017-07-01 19:48:26 +0200

    Reorganize directories, add dist/ folder

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         path: __dirname,
-        filename: "./js/bundle.js"
+        filename: "./dist/js/bundle.js"
     },
     plugins: [
         // Minimization in production
@@ -20,14 +20,14 @@ module.exports = {
             // Loader for custom tag
             { test: /\.tag\.html$/, exclude: /node_modules/, loader: 'riot-tag-loader' },
             // Loaders for stylesheet (output in css/ dir)
-            { test: /\.css$/, loader: "style!css?name=./css/[name].[ext]" },
+            { test: /\.css$/, loader: "style!css?name=./dist/css/[name].[ext]" },
             // Loaders for fonts (output in assets/ dir)
-            { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff&name=./assets/[name]/[hash].[ext]' },
-            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream&name=./assets/[name]/[hash].[ext]' },
-            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?name=./assets/[name]/[hash].[ext]' },
-            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml&name=./assets/[name]/[hash].[ext]' },
+            { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff&name=./dist/assets/[name].[ext]' },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream&name=./dist/assets/[name].[ext]' },
+            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader?name=./dist/assets/[name].[ext]' },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml&name=./dist/assets/[name].[ext]' },
             // Loader for images (output in assets/ dir)
-            { test: /\.(png|jpg|gif)$/, loader: "file-loader?name=./assets/[name].[ext]" },
+            { test: /\.(png|jpg|gif)$/, loader: "file-loader?name=./dist/assets/[name].[ext]" },
         ]
     },
     node: {
